72a34096523d72b49c7835dbc272f23048a3a54b
unknown <christineilagan02@gmail.com>
2022-02-07 21:31:11 +0800

    the program still fixing. I add some pdf encryted and I also fixed and arranged the ones in the json file

--- resume.py
+++ resume.py
@@ -1,13 +1,14 @@
+
 from fpdf import FPDF
 
-title = 'CHRISTINE ILAGAN'
-fn='ILAGAN_CHRISTINE.pdf'
+title = "CHRISTINE S. ILAGAN"
+fn="ILAGAN_CHRISTINE.pdf"
 jsonFile = "resume.json"
 
 class PDF(FPDF):
     def header(self):
         # Arial bold 15
-        self.set_font('arial', 'B', 32)
+        self.set_font('helvetica', 'B', 32)
         # Calculate width of title and position
         w = self.get_string_width(title) + 6
         self.set_x((255 - w) / 2)
@@ -22,14 +23,18 @@ class PDF(FPDF):
         # Line break
         self.ln(10)
     
+    def add_image(self):
+        #Add image
+        self.image("2X2 PIC.jpg",10, 10, 50, 0)
+    
     def chapter_body(self, name):
         # Read text file
         with open(jsonFile) as fh:
-            txt = fh.read()
+            jh = fh.read()
         # Times 12
         self.set_font('helvetica', '', 12)
         # Output justified text
-        self.multi_cell(0, 5, txt)
+        self.multi_cell(0, 5, jh)
         # Line break
         self.ln(5)
         # Mention in italics
@@ -39,7 +44,7 @@ class PDF(FPDF):
         self.add_page()
         # self.chapter_title(num, title)
         self.chapter_body(name)
-
+        self.add_image()
 
 
 pdf = PDF()
@@ -47,3 +52,32 @@ pdf.set_title(title)
 pdf.print_chapter(1, "", str(jsonFile))
 
 pdf.output(fn)
+
+
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+def add_encryption(input_pdf, output_pdf, password):
+
+    pdf_writer = PdfFileWriter()
+
+    pdf_reader = PdfFileReader(input_pdf)
+
+    for page in range(pdf_reader.getNumPages()):
+
+        pdf_writer.addPage(pdf_reader.getPage(page))
+
+    pdf_writer.encrypt(user_pwd=password, owner_pwd=None, 
+
+                       use_128bit=True)
+
+    with open(output_pdf, 'wb') as fh:
+
+        pdf_writer.write(fh)
+
+if __name__ == '__main__':
+
+    add_encryption(input_pdf='ILAGAN_CHRISTINE.pdf',
+
+                   output_pdf='ILAGAN_CHRISTINE.pdf',
+
+                   password='Cuteko02')
