b411f9b1ed28cbb5605f5683d92a40a393e15603
unknown <christineilagan02@gmail.com>
2022-02-09 23:07:42 +0800

    I made a few minor changes to the json file and now I'm finished with the objectives section.

--- resume.py
+++ resume.py
@@ -1,7 +1,7 @@
 from fpdf import FPDF
 import json
 
-title = "CHRISTINE S. ILAGAN"
+title = "CHRISTINE SALV. ILAGAN"
 
 
 class PDF(FPDF):   
@@ -9,7 +9,7 @@ class PDF(FPDF):
         #Add image
         self.image("2X2.jpg",10, 10, 50, 0)
         # Arial bold 15
-        self.set_font('helvetica', 'B', 32)
+        self.set_font('Arial', 'B', 30)
         # Calculate width of title and position
         w = self.get_string_width(title) + 6
         self.set_x((255 - w) / 2)
@@ -27,13 +27,26 @@ info = json.loads(jh)
 
 for information in info:
     pdf.ln(5)
-    pdf.set_font('helvetica', 'BI', 18)
+    pdf.set_font('Arial', 'BI', 18)
     pdf.cell(0, 10, f"{information['header1']}", 'BI', ln=1)
     pdf.ln(3)
-    pdf.set_font('helvetica', '', 12)
-    pdf.cell(0, 5, f"NAME: {information['Name']}", align='L', ln=1)
-    pdf.cell(0, 5, f"ADDRESS: {information['Address']}", align='L', ln=1)
-    pdf.cell(0, 5, f"CONTACT NO.: {information['Contact No.']}", align='L', ln=1)
-    pdf.cell(0, 5, f"EMAIL: {information['Email']}", align='L', ln=1)
+    pdf.set_font('Times', '', 12)
+    pdf.cell(0, 5, f"Name: {information['Name']}", align='L', ln=1)
+    pdf.cell(0, 5, f"Address: {information['Address']}", align='L', ln=1)
+    pdf.cell(0, 5, f"Contact No.: {information['Contact No.']}", align='L', ln=1)
+    pdf.cell(0, 5, f"Email: {information['Email']}", align='L', ln=1)
+    pdf.ln(5)
+    pdf.set_font('Arial', 'BI', 18)
+    pdf.cell(0, 10, f"{information['header2']}", 'BI', ln=1)
+    pdf.ln(3)
+    pdf.set_font('Times', '', 12)
+    pdf.cell(0, 5, f"{information['Objectives']}", align='L', ln=1)
+    pdf.cell(0, 5, f"{information['Objectives1']}", align='L', ln=1)
+    pdf.cell(0, 5, f"{information['Objectives2']}", align='L', ln=1)
+    pdf.ln(5)
+    pdf.set_font('helvetica', 'BI', 18)
+    pdf.cell(0, 10, f"{information['header3']}", 'BI', ln=1)
+
+
 
 pdf.output('ILAGAN_CHRISTINE.pdf')
